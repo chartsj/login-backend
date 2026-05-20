c6739cbcef82c1c81573cf7de76019e3d09640bb
Char Tan <chartsj@outlook.com>
2026-05-20 14:08:24 +0800

    second commit

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ pip install -r requirements.txt
 
 # Command to run FastAPI backend service 
 uvicorn main:app --reload
+
+
+# pip installations and their version
+pip install passlib 
+pip install bcrypt==4.0.1
+
+
+# steps to run this backend application
+step 1: Go into the Fastapi Login Backend Folder
+step 2: source .venv/Scripts/activate
+step 3: uvicorn main:app --reload
+step 4: Use postman to query the api endpoints
+step 5: Use DB Browser to view data in sqlite database (aka login.db)
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel
+from database import engine, Base, SessionLocal
+import models
+from models import User
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
@@ -28,17 +37,47 @@ class SignupRequest(BaseModel):
     password:str
 # endpoint for creating acct
 
-def email_exists(email):
-    list_existing_email=['test@gmail.com','123@outlook.com']
-    if email in list_existing_email:
+
+def get_db():
+    #SessionLocal
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+
+def email_exists(email: str, db:Session):
+    #query the sqlite database check if such a user exist, get a response. then return true or false accordingly
+    existing_user=db.query(User).filter(User.username==email).first()
+    
+    if existing_user:
         return True
     return False
 
+
+
 @app.post("/register", status_code=status.HTTP_201_CREATED)
-def create_acct(data:SignupRequest):
-    if email_exists(data.email):
+def create_acct(data:SignupRequest,  db:Session=Depends(get_db)):
+    if email_exists(data.email,db):
         raise HTTPException(status_code=409, detail="Email already registered")
-    if data.email and data.password:
-        return {"message":"Successfully created an account"}
+    
+    #hash password
+    pwd_context = CryptContext(
+        schemes=["bcrypt"],
+        deprecated="auto"
+    )
+
+    def hash_password(password: str):
+        return pwd_context.hash(password)
+
+    hashed_pw=hash_password(data.password)
+
+    new_user = User(username = data.email, password = hashed_pw)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message":"Successfully created an account"}
     
     
